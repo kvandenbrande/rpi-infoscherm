bfbe56c6fe9e331cff41510878ea1f8385db5c4c
Kevin Van den Brande <kevin@localhost.localdomain>
2017-10-14 17:20:23 +0200

    initial

---
+++ infoscherm.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+import time
+import sys
+import os
+import subprocess
+import json
+from subprocess import Popen
+from pygame.locals import *
+
+pygame.init()
+
+# configfile
+##try:
+##    with open('/home/pi/Desktop/infoscherm_conf.json', 'r') as f:
+##        config = json.load(f)
+##except ValueError:
+##     print 'Error: Decoding config has failed'
+##
+##LOCATION = config['LOCATION']['vestiging']
+##LOCATION_DETAIL = config['LOCATION']['locatie']
+
+# run check
+try:
+    with open("./online.tmp", "w") as processid:
+        processid.write(str(os.getpid()))
+except IOError:
+    print "Error: online.tmp cannot be made."
+
+STARTUP = pygame.font.SysFont('monospace', 25)
+TEXT = pygame.font.SysFont('roboto', 35, bold=True)
+TITLE = pygame.font.SysFont('roboto', 50, bold=True)
+TITLE1 = pygame.font.SysFont('roboto', 75, bold=True)
+WHITE = (255, 255, 255)
+WAITTIME = 30  # default time to wait between images (in seconds)
+THICKNESS = 40
+
+# media files
+LOCALEPATH = '/home/pi/infoscherm/img/'
+
+# set up the window, max screensize, fullscreen no frames
+
+modes = pygame.display.list_modes()
+screen = pygame.display.set_mode(max(modes), pygame.NOFRAME)
+pygame.mouse.set_visible(False)
+(x, y) = screen.get_size()
+
+# locations
+# TV Waasland resolutie 1920x1080 as refence to other resolutions
+
+L_STARTUP_X = 700 * x / 1920
+L_STARTUP_Y = 540 * y / 1080
+L_WELCOME_X = 150 * x / 1920
+L_WELCOME_Y = 800 * y / 1080
+L_LOGO_X = 1350 * x / 1920
+L_LOGO_Y = 800 * y / 1080
+L_BORDER_AX = 100 * x / 1920
+L_BORDER_AY = 100 * y / 1080
+L_BORDER_BX = 1600 * x / 1920
+L_BORDER_CY = 900 * y / 1080
+L_EVENT_X = 150 * x / 1920
+L_EVENTN_Y = 300 * y / 1080
+L_EVENTT_Y = 500 * y / 1080
+L_EVENTL_Y = 700 * y / 1080
+L_KA_TITEL_X = 150 * x / 1920
+L_KA_TITEL_Y = 150 * y / 1080
+
+
+# startup screen
+
+label = STARTUP.render('... warming up, please wait ...', 5, WHITE)
+screen.blit(label, (L_STARTUP_X, L_STARTUP_Y))
+pygame.display.flip()
+
+# image config
+
+logo = pygame.image.load(LOCALEPATH + 'logo.png').convert_alpha()
+sms = pygame.image.load(LOCALEPATH + '8876.jpg')
+sms = sms.convert()
+sms = pygame.transform.scale(sms, max(modes))
+
+
+# run the loop
+
+while True:
+    # startscherm
+    
+    # tekenen van welkom scherm ipv afbeelding
+    
+    screen.fill(0, 0, 0)
+
+    pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1,
+                     L_BORDER_AY), (L_BORDER_BX, L_BORDER_AY),
+                     THICKNESS)
+    pygame.draw.line(screen, WHITE, (L_BORDER_AX, L_BORDER_AY),
+                     (L_BORDER_AX, L_BORDER_CY), THICKNESS)
+    pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1,
+                     L_BORDER_CY), (L_BORDER_BX, L_BORDER_CY),
+                     THICKNESS)
+    pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2,
+                     L_BORDER_AY), (L_BORDER_BX - THICKNESS / 2,
+                     L_BORDER_CY), THICKNESS)
+
+
+
+    # add logo
+    screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y, 128, 128))
+    pygame.display.flip()
+    time.sleep(WAITTIME)
+
+    # events
+    # if check connection to mysql else goto reclame
+    # if check event available in mysql else goto reclame
+    # for each event teken event scherm
+    
+    teller = 0
+    count = 0
+    try:
+        (events, count) = get_events(LOCATION,LOCATION_DETAIL)
+    except:
+        pass
+     
+    while count > 0:
+        screen.fill(load_background())
+        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_AY),(L_BORDER_BX, L_BORDER_AY), THICKNESS)
+        pygame.draw.line(screen, WHITE, (L_BORDER_AX,L_BORDER_AY), (L_BORDER_AX,L_BORDER_CY), THICKNESS)
+        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_CY),(L_BORDER_BX, L_BORDER_CY), THICKNESS)
+        pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2, L_BORDER_AY),(L_BORDER_BX - THICKNESS / 2,L_BORDER_CY), THICKNESS)
+
+        # render text
+        titelevent = TITLE.render(events[teller][0], 5, WHITE)
+        screen.blit(titelevent, (L_EVENT_X, L_EVENTN_Y))
+        starttime = TITLE.render(events[teller][1], 5, WHITE)
+        screen.blit(starttime, (L_EVENT_X, L_EVENTT_Y))
+        location = TITLE.render(events[teller][2], 5, WHITE)
+        screen.blit(location, (L_EVENT_X, L_EVENTL_Y))
+
+        # add logo
+        screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y,128, 128))
+        pygame.display.flip()
+        time.sleep(WAITTIME)
+        count = count - 1
+        teller = teller + 1
+
+    # reclame 8876
+    if LOCATION_DETAIL != 'Auditorium':
+        screen.blit(sms, (0, 0))
+        pygame.display.flip()
+        time.sleep(WAITTIME)
+
+

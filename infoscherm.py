69aaa5a8ecb6133229c0bc727679101b8af06dd7
Kevin Van den Brande <kevin@localhost.localdomain>
2017-10-15 14:00:03 +0200

    update

--- infoscherm.py
+++ infoscherm.py
@@ -34,7 +34,7 @@ TITLE = pygame.font.SysFont('roboto', 50, bold=True)
 TITLE1 = pygame.font.SysFont('roboto', 75, bold=True)
 WHITE = (255, 255, 255)
 WAITTIME = 30  # default time to wait between images (in seconds)
-THICKNESS = 40
+THICKNESS = 10
 
 # media files
 LOCALEPATH = '/home/pi/infoscherm/img/'
@@ -75,10 +75,20 @@ pygame.display.flip()
 
 # image config
 
-logo = pygame.image.load(LOCALEPATH + 'logo.png').convert_alpha()
-sms = pygame.image.load(LOCALEPATH + '8876.jpg')
-sms = sms.convert()
-sms = pygame.transform.scale(sms, max(modes))
+#transparant = pygame.image.load(LOCALEPATH + 'logo.png').convert_alpha()
+img1 = pygame.image.load(LOCALEPATH + '1.jpg')
+img1 = img1.convert()
+img1 = pygame.transform.scale(img1, max(modes))
+img2 = pygame.image.load(LOCALEPATH + '5.jpg')
+img2 = img2.convert()
+img2 = pygame.transform.scale(img2, max(modes))
+img3 = pygame.image.load(LOCALEPATH + '6.jpg')
+img3 = img3.convert()
+img3 = pygame.transform.scale(img3, max(modes))
+img4 = pygame.image.load(LOCALEPATH + '9.jpg')
+img4 = img4.convert()
+img4 = pygame.transform.scale(img4, max(modes))
+
 
 
 # run the loop
@@ -90,63 +100,65 @@ while True:
     
     screen.fill(0, 0, 0)
 
-    pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1,
-                     L_BORDER_AY), (L_BORDER_BX, L_BORDER_AY),
-                     THICKNESS)
-    pygame.draw.line(screen, WHITE, (L_BORDER_AX, L_BORDER_AY),
-                     (L_BORDER_AX, L_BORDER_CY), THICKNESS)
-    pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1,
-                     L_BORDER_CY), (L_BORDER_BX, L_BORDER_CY),
-                     THICKNESS)
-    pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2,
-                     L_BORDER_AY), (L_BORDER_BX - THICKNESS / 2,
-                     L_BORDER_CY), THICKNESS)
+##    pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1,
+##                     L_BORDER_AY), (L_BORDER_BX, L_BORDER_AY),
+##                     THICKNESS)
+##    pygame.draw.line(screen, WHITE, (L_BORDER_AX, L_BORDER_AY),
+##                     (L_BORDER_AX, L_BORDER_CY), THICKNESS)
+##    pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1,
+##                     L_BORDER_CY), (L_BORDER_BX, L_BORDER_CY),
+##                     THICKNESS)
+##    pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2,
+##                     L_BORDER_AY), (L_BORDER_BX - THICKNESS / 2,
+##                     L_BORDER_CY), THICKNESS)
 
 
-
-    # add logo
-    screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y, 128, 128))
-    pygame.display.flip()
-    time.sleep(WAITTIME)
 
     # events
     # if check connection to mysql else goto reclame
     # if check event available in mysql else goto reclame
     # for each event teken event scherm
     
-    teller = 0
-    count = 0
-    try:
-        (events, count) = get_events(LOCATION,LOCATION_DETAIL)
-    except:
-        pass
-     
-    while count > 0:
-        screen.fill(load_background())
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_AY),(L_BORDER_BX, L_BORDER_AY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX,L_BORDER_AY), (L_BORDER_AX,L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_CY),(L_BORDER_BX, L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2, L_BORDER_AY),(L_BORDER_BX - THICKNESS / 2,L_BORDER_CY), THICKNESS)
+##    teller = 0
+##    count = 0
+##    try:
+##        (events, count) = get_events(LOCATION,LOCATION_DETAIL)
+##    except:
+##        pass
+##     
+##    while count > 0:
+##        screen.fill(load_background())
+##        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_AY),(L_BORDER_BX, L_BORDER_AY), THICKNESS)
+##        pygame.draw.line(screen, WHITE, (L_BORDER_AX,L_BORDER_AY), (L_BORDER_AX,L_BORDER_CY), THICKNESS)
+##        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_CY),(L_BORDER_BX, L_BORDER_CY), THICKNESS)
+##        pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2, L_BORDER_AY),(L_BORDER_BX - THICKNESS / 2,L_BORDER_CY), THICKNESS)
+##
+##        # render text
+##        titelevent = TITLE.render(events[teller][0], 5, WHITE)
+##        screen.blit(titelevent, (L_EVENT_X, L_EVENTN_Y))
+##        starttime = TITLE.render(events[teller][1], 5, WHITE)
+##        screen.blit(starttime, (L_EVENT_X, L_EVENTT_Y))
+##        location = TITLE.render(events[teller][2], 5, WHITE)
+##        screen.blit(location, (L_EVENT_X, L_EVENTL_Y))
+##
+##        # add logo
+##        screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y,128, 128))
+##        pygame.display.flip()
+##        time.sleep(WAITTIME)
+##        count = count - 1
+##        teller = teller + 1
 
-        # render text
-        titelevent = TITLE.render(events[teller][0], 5, WHITE)
-        screen.blit(titelevent, (L_EVENT_X, L_EVENTN_Y))
-        starttime = TITLE.render(events[teller][1], 5, WHITE)
-        screen.blit(starttime, (L_EVENT_X, L_EVENTT_Y))
-        location = TITLE.render(events[teller][2], 5, WHITE)
-        screen.blit(location, (L_EVENT_X, L_EVENTL_Y))
-
-        # add logo
-        screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y,128, 128))
-        pygame.display.flip()
-        time.sleep(WAITTIME)
-        count = count - 1
-        teller = teller + 1
-
-    # reclame 8876
-    if LOCATION_DETAIL != 'Auditorium':
-        screen.blit(sms, (0, 0))
-        pygame.display.flip()
-        time.sleep(WAITTIME)
-
+    # images
+    screen.blit(img1, (0, 0))
+    pygame.display.flip()
+    time.sleep(WAITTIME)
+    screen.blit(img2, (0, 0))
+    pygame.display.flip()
+    time.sleep(WAITTIME)
+    screen.blit(img3, (0, 0))
+    pygame.display.flip()
+    time.sleep(WAITTIME)
+    screen.blit(img4, (0, 0))
+    pygame.display.flip()
+    time.sleep(WAITTIME)
 
